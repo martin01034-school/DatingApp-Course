cdc693d57d62335f2bf4afab36855adc6a2c3184
Martin Wögerbauer <20162034@atn.ac.at>
2022-01-15 09:35:13 +0100

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,9 +5,13 @@ using System.Threading.Tasks;
 
 namespace API.Entities
 {
+    
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
     }
 }
